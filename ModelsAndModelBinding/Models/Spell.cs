b758f9b99f1be833e14f884497e0347ce91cd0e0
France A <133005536+fafafafrance@users.noreply.github.com>
2026-08-08 09:50:50 -0700

    Add data annotations to Spell class for validation
    Added [Required] and [StringLength] to Name property in Spell class. Introduced SpellId property and updated using directives for data annotations. Ensured Name is a required string for improved validation.

--- ModelsAndModelBinding/Models/Spell.cs
+++ ModelsAndModelBinding/Models/Spell.cs
@@ -1,9 +1,17 @@
-﻿namespace ModelsAndModelBinding.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelsAndModelBinding.Models
 {
     /// <summary>
     /// Represents an individual magical spell in the grimoire, containing its attributes, level, and mana requirements for casting.
     /// </summary>
     public class Spell
     {
+        public int SpellId { get; set; }
+
+        [Required(ErrorMessage = "Please enter the name of the spell.")]
+        [StringLength(40, ErrorMessage = "Spell name cannot exceed 40 characters")]
+        public required string Name { get; set; }
+
     }
 }
