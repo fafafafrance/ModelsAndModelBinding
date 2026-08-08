e81972ceff98c0132b5c8799370f1c590a7974e4
France A <133005536+fafafafrance@users.noreply.github.com>
2026-08-08 09:56:44 -0700

    Add School, level, ManaCost to Spell with validation
    Added School, level, and ManaCost properties to the Spell class, each with validation attributes. Improved error messages for Name and School properties for consistency.

--- ModelsAndModelBinding/Models/Spell.cs
+++ ModelsAndModelBinding/Models/Spell.cs
@@ -10,8 +10,18 @@ namespace ModelsAndModelBinding.Models
         public int SpellId { get; set; }
 
         [Required(ErrorMessage = "Please enter the name of the spell.")]
-        [StringLength(40, ErrorMessage = "Spell name cannot exceed 40 characters")]
+        [StringLength(40, ErrorMessage = "Spell name cannot exceed 40 characters.")]
         public required string Name { get; set; }
+
+        [Required(ErrorMessage = "Please specify the school of magic.")]
+        [StringLength(30, ErrorMessage = "School of magic cannot exceed 30 characters.")]
+        public required string School {  get; set; }
+
+        [Range(0, 9, ErrorMessage = "Spell level must be between 0 and 9")]
+        public int level { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Mana cost must be between 0 and 100")]
+        public int ManaCost {  get; set; }
 
     }
 }
