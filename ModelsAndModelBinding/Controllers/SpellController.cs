52833beeea1273035ad6d38f9a8603d6881679ca
France A <133005536+fafafafrance@users.noreply.github.com>
2026-08-08 10:48:29 -0700

    Add Create action to SpellController for new Spell form
    Added GET and POST Create actions to SpellController to handle displaying and processing the new Spell form. Included model validation, success messaging, redirection, and a using directive for ModelsAndModelBinding.Models.

--- ModelsAndModelBinding/Controllers/SpellController.cs
+++ ModelsAndModelBinding/Controllers/SpellController.cs
@@ -1,9 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ModelsAndModelBinding.Models;
 
 namespace ModelsAndModelBinding.Controllers
 {
     public class SpellController : Controller
     {
+        // Get: /Spell/Create
+        [HttpGet]
+        public IActionResult Create()
+        {  
+            return View(); 
+        }
+
+        // Post: /Spell/Create
+        [HttpPost]
+        public IActionResult Create(Spell spell)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add spell to the database (SpellDb.Add(spell);)
+                TempData["SuccessMessage"] = $"{spell.Name} (Level {spell.Level}, Rarity Tier {spell.Rarity}) was successfully inscribed into the grimoire!";
+                return RedirectToAction("Index");
+            }
+
+            return View(spell);
+
+        }
+
+        // Get: /Spell/Index
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
