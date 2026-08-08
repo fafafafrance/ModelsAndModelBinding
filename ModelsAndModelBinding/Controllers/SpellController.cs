a4cc8e04c329963d432f0d6861168c800a8c2306
France A <133005536+fafafafrance@users.noreply.github.com>
2026-08-08 10:29:41 -0700

    Add SpellController with Index action
    Added SpellController to ModelsAndModelBinding.Controllers, inheriting from Controller. Included Index action returning a View and necessary using directive for Microsoft.AspNetCore.Mvc.

---
+++ ModelsAndModelBinding/Controllers/SpellController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelsAndModelBinding.Controllers
+{
+    public class SpellController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
